ee7228f17a770386a7750d5bf6ff40ff8008bd19
JunkTurkey <30864652+JunkTurkey@users.noreply.github.com>
2018-09-13 15:51:02 +0300

    telegram notification done

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,8 +2,10 @@
 
 namespace App\Console;
 
+use App\Notifications\PlannedNotification;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Notification;
 
 class Kernel extends ConsoleKernel
 {
@@ -24,7 +26,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
+         $schedule->call(function (){
+             Notification::send('', new PlannedNotification());
+         })->everyMinute();
         //          ->hourly();
     }
 
